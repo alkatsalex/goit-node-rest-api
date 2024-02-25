ca8304d3efb7bcb284e6ba48401fcab0428876ed
alkatsalex <alkatsalex@ukr.net>
2024-02-25 15:11:43 +0200

    done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,77 @@
 import contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const data = await contactsService.listContacts();
+    console.log(typeof data);
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const id = req.params.id;
 
-export const deleteContact = (req, res) => {};
+  try {
+    const data = await contactsService.getContactById(id);
+    console.log(data);
+    if (typeof data === "undefined") {
+      res.status(404).json({ message: "Not found contact" });
+    }
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const data = await contactsService.removeContact(id);
+    if (typeof data === "undefined") {
+      res.status(404).json({ message: "Not found contact" });
+    }
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  const { _, error } = createContactSchema.validate({ name, email, phone });
+
+  if (typeof error !== "undefined") {
+    return res.status(400).json({ message: error.message });
+  }
+
+  try {
+    const data = await contactsService.addContact(name, email, phone);
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, email, phone } = req.body;
+  const { value, error } = updateContactSchema.validate({ name, email, phone });
+
+  if (typeof error !== "undefined") {
+    return res.status(400).json({ message: error.message });
+  }
+
+  try {
+    const data = await contactsService.updateContact(id, value);
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,95 @@
+import fs from "node:fs/promises";
+import crypto from "node:crypto";
+import path from "node:path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "../db/contacts.json");
+
+async function readContacts() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+}
+
+async function writeContacts(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+}
+
+async function listContacts() {
+  const contacts = await readContacts();
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return undefined;
+  }
+
+  const deletedBook = contacts[index];
+
+  contacts.splice(index, 1);
+
+  await writeContacts(contacts);
+
+  return deletedBook;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await readContacts();
+
+  const newContact = {
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+
+  await writeContacts(contacts);
+  return newContact;
+}
+
+async function updateContact(id, newParams) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+
+  if (index === -1) {
+    return undefined;
+  }
+
+  const updatedContact = { ...newParams, id };
+
+  contacts[index] = updatedContact;
+
+  await writeContacts(contacts);
+
+  return updatedContact;
+}
+
+listContacts().catch((error) => console.error(error));
+
+const contactsService = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
+
+export default contactsService;
